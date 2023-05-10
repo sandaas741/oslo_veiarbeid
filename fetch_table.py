fa8f12d314ab4b3fd4512f936d131dc04cd22f16
ottaas <ottar.aas@varenergi.no>
2023-05-10 23:48:43 +0200

    first commit

---
+++ fetch_table.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from datetime import datetime
+
+@st.cache_data
+def load_data(file):
+    df = pd.read_csv(file)  
+    return df
+
+st.title("Oslo Vårrengjøring 2023")
+df = load_data('vaarengjoring.csv')
+
+
+# Create the date range dropboxes
+min_date = pd.to_datetime(df['Dato'], format='%d.%m.%Y').min()
+max_date = pd.to_datetime(df['Dato'], format='%d.%m.%Y').max()
+print(min_date, max_date)
+
+filter_street = st.sidebar.checkbox('Filter on street:')
+
+if filter_street:
+    search_term = st.sidebar.text_input('Search Address')
+    if search_term:
+        df = df.loc[df['Gatenavn'].str.contains(search_term, case=False)]
+    else:
+        df = df
+
+
+# Filter the data based on the date range
+filter_date = st.sidebar.checkbox('Filter on date:')
+
+if filter_date:
+    date_range = st.sidebar.date_input('Date:', max_date, min_value=min_date, max_value=max_date)
+    df = df[(pd.to_datetime(df['Dato'], format='%d.%m.%Y').dt.date == date_range)]
+else:
+    df = df
+
+
+
+
+
+
+# Update the map with the filtered data
+fig1 = px.scatter_mapbox(df, lat='lat', lon='lon', hover_name='Gatenavn', hover_data =["Kommentar", "Tidpunkt"], zoom=10, color_discrete_sequence=["fuchsia"])
+fig1.update_layout(mapbox_style="open-street-map")
+fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+# Display the updated map in Streamlit
+st.plotly_chart(fig1)
+st.write(df.iloc[:,0:4])
